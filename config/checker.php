fd3f0d011178dd84a93cb7d82f8435c40903e7be
abusan3225 <m.aburaya1014@gmail.com>
2023-06-01 17:37:29 +0900

    config を env から読み込むように変更

--- config/checker.php
+++ config/checker.php
@@ -1,11 +1,17 @@
 <?php
 
-// 設定を記述します
+// 設定はenvに記述して読み込む
 // ディレクトリは末尾スラッシュ無し
+// DISCORD_CSV_PATH
+//   Discord から dump した csv ファイルのフルパス
+// CHECK_RESULT_FILE_PATH
+//   結果を出力する csv ファイルを置くディレクトリ
+// CHECK_RESULT_FILE_NAME
+//   結果を出力する csv ファイルの名前
 
 return [
-    'csvPath'          => '',   // DiscordからダンプしたCSVファイルのフルパス
-    'resultOutputPath' => '',  // 結果ファイルの出力先ディレクトリパス
-    'resultFilename'   => 'check_%d.csv', // 出力ファイル名(%d = 日付)
+    'csvPath'          => env('DISCORD_CSV_PATH', ''),   // DiscordからダンプしたCSVファイルのパス
+    'resultOutputPath' => env('CHECK_RESULT_FILE_PATH', ''),  // 結果ファイルの出力パス
+    'resultFilename'   => env('CHECK_RESULT_FILE_NAME', ''), // 出力ファイル名(%d = 日付)
     'sleepMicrotime'   => 300000,  // 1回の実行ごとの待機時間(マイクロ秒、1秒 = 1000000)
 ];
